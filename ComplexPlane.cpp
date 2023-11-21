7afdbe733fc5d20b13396541dfa499e0a9677709
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-21 14:01:57 -0800

    Update ComplexPlane.cpp

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,1 +1,54 @@
 
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+{
+  
+}
+
+
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
+{
+  target.draw(m_vArray);
+}
+
+
+void complexPlane::updateRender()
+{
+  
+}
+
+void ComplexPlane::zoomIn()
+{
+  
+}
+
+void ComplexPlane::zoomOut()
+{
+  
+}
+
+void ComplexPlane::setCenter(Vector2i mousePixel)
+{
+
+}
+
+void ComplexPlane::setMouseLocation(Vector2i mousPixel)
+{
+  
+  size_t ComplexPlane::countIterations(Vector2f coord)
+}
+
+void ComplexPlane::loadText(Text& text)
+{
+
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
+{
+
+}
+
+Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
+{
+  
+}
+
