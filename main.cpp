26cd60c3ac5e8914ac1998f0ae7d211e28e3d031
alexwoodn <150198820+alexwoodn@users.noreply.github.com>
2023-11-22 19:31:55 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,55 @@
 #include <SFML/Graphics.hpp>
-#include "ComplexPlane.h"
+#include "ComplexPlane.cpp"
+
+using namespace sf;
+using namespace std;
 
 int main() {
     // Get desktop resolution
-    unsigned int screenWidth = sf::VideoMode::getDesktopMode().width / 2;
-    unsigned int screenHeight = sf::VideoMode::getDesktopMode().height / 2;
+    unsigned int screenWidth = VideoMode::getDesktopMode().width / 2;
+    unsigned int screenHeight = VideoMode::getDesktopMode().height / 2;
 
     // Construct the RenderWindow
-    sf::RenderWindow window(sf::VideoMode(screenWidth, screenHeight), "Complex Plane Visualization");
+    RenderWindow window(VideoMode(screenWidth, screenHeight), "Complex Plane Visualization");
 
     // Construct the ComplexPlane
     ComplexPlane complexPlane(screenWidth, screenHeight);
 
     // Construct the Font and Text objects
-    sf::Font font;
+    Font font;
     if (!font.loadFromFile("arial.ttf")) {
         // Handle font loading error
         return EXIT_FAILURE;
     }
 
-    sf::Text text;
+    Text text;
     text.setFont(font);
     text.setCharacterSize(20);
-    text.setFillColor(sf::Color::White);
+    text.setFillColor(Color::White);
 
     // Main loop
     while (window.isOpen()) {
         // Handle Input segment
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event)) {
             switch (event.type) {
-                case sf::Event::Closed:
+                case Event::Closed:
                     window.close();
                     break;
 
-                case sf::Event::MouseButtonPressed:
-                    if (event.mouseButton.button == sf::Mouse::Right) {
+                case Event::MouseButtonPressed:
+                    if (event.mouseButton.button == Mouse::Right) {
                         // Right click to zoom out
                         complexPlane.zoomOut(event.mouseButton.x, event.mouseButton.y);
                         complexPlane.setCalculationState(ComplexPlane::CalculationState::CALCULATING);
-                    } else if (event.mouseButton.button == sf::Mouse::Left) {
+                    } else if (event.mouseButton.button == Mouse::Left) {
                         // Left click to zoom in
                         complexPlane.zoomIn(event.mouseButton.x, event.mouseButton.y);
                         complexPlane.setCalculationState(ComplexPlane::CalculationState::CALCULATING);
                     }
                     break;
 
-                case sf::Event::MouseMoved:
+                case Event::MouseMoved:
                     // Store mouse location for later use
                     complexPlane.setMouseLocation(event.mouseMove.x, event.mouseMove.y);
                     break;
@@ -57,20 +60,20 @@ int main() {
         }
 
         // Check if Escape key is pressed to close the window
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+        if (Keyboard::isKeyPressed(Keyboard::Escape)) {
             window.close();
         }
 
         // Update Scene segment
         complexPlane.updateRender();
-        complexPlane.loadText();
-        
+
+        // Load the text into the sf::Text object
+        complexPlane.loadText(text);
+
         // Draw Scene segment
         window.clear();
         window.draw(complexPlane);
-        text.setString("Mouse Coordinates: (" + std::to_string(complexPlane.getMouseX()) + ", " +
-                        std::to_string(complexPlane.getMouseY()) + ")");
-        window.draw(text);
+        window.draw(text);  // Draw the text
         window.display();
     }
 
